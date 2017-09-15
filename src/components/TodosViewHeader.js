7f8d89e0c03ae033d1b4ddb8b24748ee305df7f0
condef5 <davis.con.fab@gmail.com>
2017-09-15 12:11:48 -0500

    Fixed identation in App and TodoViewHeader

--- src/components/TodosViewHeader.js
+++ src/components/TodosViewHeader.js
@@ -4,17 +4,11 @@ import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 import '../styles/TodosViewHeader.css'
 
-const TodosViewHeader = ({ 
-	createTodo, 
-	isCreating, 
-	currentFilter, 
-	filters,
-	setFilter 
-}) => (
-	<header id="todos-view-header">
-		<section className="left">
-			<h4>TODOS</h4>
-			  <Select
+const TodosViewHeader = ({ createTodo,isCreating, currentFilter, filters, setFilter }) => (
+  <header id="todos-view-header">
+    <section className="left">
+      <h4>TODOS</h4>
+        <Select
           name="form-field-name"
           className="filters"
           value={currentFilter}
@@ -26,22 +20,22 @@ const TodosViewHeader = ({
               label: f[0] + f.slice(1).toLowerCase()
             }
           })}
-          onChange={option => { setFilter(option.value) }}
+          onChange={ option => { setFilter(option.value) }}
         />
-		</section>
-		<a 
-			className={classNames('add-todo', {
+    </section>
+    <a 
+      className={classNames('add-todo', {
           processing: isCreating
         })}
-			href="#" 
-			onClick={(e) =>{ 
-				e.preventDefault(); 
-				var todo = {'titulo': 'new Todo', descripcion: "Something", prioridad: 1, activo: false} 
-				createTodo(todo) 
-			}} 
-		>
+      href="#" 
+      onClick={(e) =>{ 
+        e.preventDefault(); 
+        var todo = {'titulo': 'new Todo', descripcion: "Something", prioridad: 1, activo: false} 
+        createTodo(todo) 
+      }} 
+    >
         <i className="fa fa-plus" aria-hidden="true"></i>
     </a>
-	</header>
+  </header>
 )
 export default TodosViewHeader
