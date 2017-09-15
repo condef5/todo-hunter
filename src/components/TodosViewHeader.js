7b4306eed33ae7163ca2ce8e65cc8a67d9acabce
condef5 <davis.con.fab@gmail.com>
2017-09-15 12:16:08 -0500

    Fixed identation TodoViewHeader

--- src/components/TodosViewHeader.js
+++ src/components/TodosViewHeader.js
@@ -5,37 +5,37 @@ import 'react-select/dist/react-select.css';
 import '../styles/TodosViewHeader.css'
 
 const TodosViewHeader = ({ createTodo,isCreating, currentFilter, filters, setFilter }) => (
-  <header id="todos-view-header">
-    <section className="left">
-      <h4>TODOS</h4>
-        <Select
-          name="form-field-name"
-          className="filters"
-          value={currentFilter}
-          clearable={false}
-          searchable={false}
-          options={filters.map(f => {
-            return {
-              value: f,
-              label: f[0] + f.slice(1).toLowerCase()
-            }
-          })}
-          onChange={ option => { setFilter(option.value) }}
-        />
-    </section>
-    <a 
-      className={classNames('add-todo', {
-          processing: isCreating
-        })}
-      href="#" 
-      onClick={(e) =>{ 
-        e.preventDefault(); 
-        var todo = {'titulo': 'new Todo', descripcion: "Something", prioridad: 1, activo: false} 
-        createTodo(todo) 
-      }} 
-    >
-        <i className="fa fa-plus" aria-hidden="true"></i>
-    </a>
-  </header>
+	<header id="todos-view-header">
+		<section className="left">
+			<h4>TODOS</h4>
+			<Select
+				name="form-field-name"
+				className="filters"
+				value={currentFilter}
+				clearable={false}
+				searchable={false}
+				options={filters.map(f => {
+					return {
+						value: f,
+						label: f[0] + f.slice(1).toLowerCase()
+					}
+				})}
+				onChange={ option => { setFilter(option.value) }}
+			/>
+		</section>
+		<a 
+			className={classNames('add-todo', {
+				processing: isCreating
+			})}
+			href="#" 
+			onClick={(e) =>{ 
+				e.preventDefault(); 
+				var todo = {'titulo': 'new Todo', descripcion: "Something", prioridad: 1, activo: false} 
+				createTodo(todo) 
+			}} 
+		>
+			<i className="fa fa-plus" aria-hidden="true"></i>
+		</a>
+	</header>
 )
 export default TodosViewHeader
