2e59bbeefb23d6f7510358d75468b359c5fa0310
condef5 <davis.con.fab@gmail.com>
2017-09-24 23:55:03 -0500

    Fixed identation

--- src/__tests__/components/TodoItem.test.js
+++ src/__tests__/components/TodoItem.test.js
@@ -3,11 +3,11 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 
 describe('TodoList component renders the todo correctly', () => {
-	it('renders correctly', () => {
-		const todo = {id: 1, activo: false, titulo: 'Soy un todo'}
-		const rendered = renderer.create(
-			<TodoItem {...todo} />
-		)
-		expect(rendered.toJSON()).toMatchSnapshot();
-	})
+  it('renders correctly', () => {
+    const todo = {id: 1, activo: false, titulo: 'Soy un todo'}
+    const rendered = renderer.create(
+      <TodoItem {...todo} />
+    )
+    expect(rendered.toJSON()).toMatchSnapshot();
+  })
 })
