058007747280548ae1d969b024c1b858f329c2c8
condef5 <davis.con.fab@gmail.com>
2017-09-24 23:52:08 -0500

    Test for actions and reducers

--- src/__tests__/components/TodoItem.test.js
+++ src/__tests__/components/TodoItem.test.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer'
 
 describe('TodoList component renders the todo correctly', () => {
 	it('renders correctly', () => {
-		const todo = {id:1, activo: false, titulo: 'Soy un todo'}
+		const todo = {id: 1, activo: false, titulo: 'Soy un todo'}
 		const rendered = renderer.create(
 			<TodoItem {...todo} />
 		)
--- src/actions/todos.js
+++ src/actions/todos.js
@@ -2,48 +2,53 @@ import axios from "axios";
 
 const baseUrl = 'https://crud-in-rails-api.herokuapp.com/api/tareas'
 
-export function fetchTodos() {
-  return {
-    type: 'FETCH_TODOS',
-    payload: axios.get(baseUrl)
-      .then(response => ({todos : response.data}) )
-  } 
+export function fetchTodos() { 
+  return function(dispatch) {
+    dispatch({type: "FETCH_TODOS_REQUEST"}); 
+    return axios.get(baseUrl)
+      .then((response) => {
+        dispatch({type: "FETCH_TODOS_SUCCESS", payload: {todos: response.data}})
+      })
+      .catch((err) => {
+        dispatch({type: "FETCH_TODOS_FAILURE" })
+      })
+  }
 }
 
 export function createTodo(todo) {
   return function(dispatch) {
-    dispatch({type: "CREATE_TODO_PENDING"}); 
-    axios.post(baseUrl, todo)
+    dispatch({type: "CREATE_TODO_REQUEST"}); 
+    return axios.post(baseUrl, todo)
       .then((response) => {
-        dispatch({type: "CREATE_TODO_FULFILLED", payload: {todo: response.data}})
+        dispatch({type: "CREATE_TODO_SUCCESS", payload: {todo: response.data}})
         dispatch(selectTodo(response.data.id)); 
       })
       .catch((err) => {
-        dispatch({type: "CREATE_TODO_REJECTED", payload: err})
+        dispatch({type: "CREATE_TODO_FAILURE"})
       })
   } 
 }
 export function updateTodo(todo) {
   return function(dispatch) {
-    dispatch({type: "UPDATE_TODO_PENDING" , payload: { id: todo.id } }); 
-    axios.put(`${baseUrl}/${todo.id}`, todo)
+    dispatch({type: "UPDATE_TODO_REQUEST" , payload: { id: todo.id } }); 
+    return axios.put(`${baseUrl}/${todo.id}`, todo)
       .then((response) => {
-        dispatch({type: "UPDATE_TODO_FULFILLED", payload: {todo: response.data}})
+        dispatch({type: "UPDATE_TODO_SUCCESS", payload: {todo: response.data}})
       })
       .catch((err) => {
-        dispatch({type: "UPDATE_TODO_REJECTED", payload: err})
+        dispatch({type: "UPDATE_TODO_FAILURE"})
       })
   }
 } 
 export function deleteTodo(id) {
   return function(dispatch) {
-    dispatch({type: "DELETE_TODO_PENDING" , payload: { id } }); 
-    axios.delete(`${baseUrl}/${id}`)
+    dispatch({type: "DELETE_TODO_REQUEST" , payload: { id } }); 
+    return axios.delete(`${baseUrl}/${id}`)
       .then((response) => {
-        dispatch({type: "DELETE_TODO_FULFILLED", payload: { id }})
+        dispatch({type: "DELETE_TODO_SUCCESS", payload: { id }})
       })
       .catch((err) => {
-        dispatch({type: "DELETE_TODO_REJECTED", payload: err})
+        dispatch({type: "DELETE_TODO_FAILURE"})
       })
   }
 }  
